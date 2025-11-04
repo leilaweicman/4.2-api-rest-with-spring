14e97fc517e7eee0105104f0d1116c98f6bf2a6f
leilaweicman <leilaweic@hotmail.com>
2025-11-04 12:28:24 +0100

    GREEN: Implemented FruitServiceImpl to pass validation tests

--- src/main/java/cat/itacademy/s04/s02/n01/fruit/services/FruitServiceImpl.java
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/services/FruitServiceImpl.java
@@ -1,12 +1,30 @@
 package cat.itacademy.s04.s02.n01.fruit.services;
 
+import cat.itacademy.s04.s02.n01.fruit.exception.InvalidFruitNameException;
+import cat.itacademy.s04.s02.n01.fruit.exception.InvalidFruitWeightException;
 import cat.itacademy.s04.s02.n01.fruit.model.Fruit;
+import cat.itacademy.s04.s02.n01.fruit.repository.FruitRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FruitServiceImpl implements FruitService{
+public class FruitServiceImpl implements FruitService {
+
+    private final FruitRepository fruitRepository;
+
+    public FruitServiceImpl(FruitRepository fruitRepository) {
+        this.fruitRepository = fruitRepository;
+    }
+
     @Override
     public Fruit createFruit(Fruit fruit) {
-        return null;
+        if (fruit.getName() == null || fruit.getName().isBlank()) {
+            throw new InvalidFruitNameException("Fruit name cannot be empty");
+        }
+        if (fruit.getWeight() <= 0) {
+            throw new InvalidFruitWeightException("Fruit weight must be positive");
+        }
+
+        return fruitRepository.save(fruit);
     }
+
 }
